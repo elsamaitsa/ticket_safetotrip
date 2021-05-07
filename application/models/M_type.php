fadd82e8b01d1a032ffdd5382ea837a9e33484e2
vayoreto <vayoretoevergaden@gmail.com>
2021-05-07 15:59:48 +0700

    crud type/varian

---
+++ application/models/M_type.php
@@ -0,0 +1,40 @@
+<?php
+defined('BASEPATH') or die('Tidak Boleh Akses Langsung');
+
+class M_type extends CI_model
+{
+
+    public function view_type()
+    {
+        $this->db->group_by('rvarianticket_id');
+        $sql = $this->db->get('r_varianticket');
+        if ($sql->num_rows() > 0) {
+            return $sql->result_array();
+        }
+    }
+
+    public function Insert()
+    {
+        $data = array(
+            'rvarianticket_type' => $_POST['type'],
+            'rvarianticket_nama' => $_POST['nama'],
+        );
+        $this->db->insert('r_varianticket', $data);
+    }
+
+    public function Delete($rvarianticket_id)
+    {
+        $this->db->where('rvarianticket_id', $rvarianticket_id);
+        $this->db->delete('r_varianticket');
+    }
+
+    public function Update()
+    {
+        $data = array(
+            'rvarianticket_type' => $_POST['type'],
+            'rvarianticket_nama' => $_POST['nama'],
+        );
+        $this->db->where('rvarianticket_id',  $_POST['rvarianticket_id']);
+        $this->db->update('r_varianticket', $data);
+    }
+}
--- application/views/template/header.php
+++ application/views/template/header.php
@@ -1,36 +1,38 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>Admin - Safe2Trip</title>
-  <link rel="icon" type="image/png" href="<?php echo base_url()?>assets/image/user-ava.png">
+  <link rel="icon" type="image/png" href="<?php echo base_url() ?>assets/image/user-ava.png">
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
-  <link rel="stylesheet" href="<?php echo base_url();?>assets/plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/fontawesome-free/css/all.min.css">
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="<?php echo base_url();?>assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
   <!-- iCheck -->
-  <link rel="stylesheet" href="<?php echo base_url();?>assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/icheck-bootstrap/icheck-bootstrap.min.css">
   <!-- JQVMap -->
-  <link rel="stylesheet" href="<?php echo base_url();?>assets/plugins/jqvmap/jqvmap.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/jqvmap/jqvmap.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="<?php echo base_url();?>assets/dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/adminlte.min.css">
   <!-- Add CSS -->
-  <link rel="stylesheet" href="<?php echo base_url();?>assets/dist/css/custom.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/dist/css/custom.css">
   <!-- overlayScrollbars -->
-  <link rel="stylesheet" href="<?php echo base_url();?>assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
   <!-- Daterange picker -->
-  <link rel="stylesheet" href="<?php echo base_url();?>assets/plugins/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/daterangepicker/daterangepicker.css">
   <!-- summernote -->
-  <link rel="stylesheet" href="<?php echo base_url();?>assets/plugins/summernote/summernote-bs4.min.css">
+  <link rel="stylesheet" href="<?php echo base_url(); ?>assets/plugins/summernote/summernote-bs4.min.css">
   <!-- Datatable -->
-  <link rel="stylesheet" href="<?php echo base_url()?>assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
-  <link rel="stylesheet" href="<?php echo base_url()?>assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" href="<?php echo base_url() ?>assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 
   <!-- jQuery -->
-<script src="<?=base_url()?>assets/plugins/jquery/jquery.min.js"></script>
+  <script src="<?= base_url() ?>assets/plugins/jquery/jquery.min.js"></script>
 </head>
