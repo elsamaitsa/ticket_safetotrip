a55b7272eae71e9366600871ef29682dc15ee2ac
AyuMuhafilah <ayumuhafilah10@gmail.com>
2021-05-07 15:46:09 +0700

    ay-add model M_group

--- application/controllers/Group.php
+++ application/controllers/Group.php
@@ -3,13 +3,52 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Group extends CI_Controller {
 
+	public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('M_group');
+    }
+
 	public function index()
 	{
+		$data['group'] = $this->M_group->all()->result_array();
+		$data['idGroup'] = ($this->M_group->all()->num_rows() + 1);
 		$this->load->view('template/header');
 		$this->load->view('template/navbar');
 		$this->load->view('template/sidebar');
-		$this->load->view('v_ListGroup');
+		$this->load->view('v_ListGroup', $data);
         $this->load->view('template/footer');
+	}
+
+	public function store()
+	{
+		$set = [
+			'rmoment_name' => $this->input->post('rmoment_name'),
+		];
+		$this->M_group->save($set);
+		redirect('Group');
+	}
+
+	public function edit(int $id)
+	{
+		$group = $this->M_group->find($id)->row_array();
+		$this->output->set_content_type('aplication/json')
+			 ->set_output(json_encode($group));
+	}
+
+	public function update(int $id)
+	{
+		$set = [
+			'rmoment_name' => $this->input->post('rmoment_name'),
+		];
+		$this->M_group->save($set, $id);
+		redirect('Group');
+	}
+
+	public function destroy(int $id)
+	{
+		$this->M_group->destroy($id);
+		redirect('Group');
 	}
 }
 
