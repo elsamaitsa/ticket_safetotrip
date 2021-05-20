d7ff8a0d65bd695e4649bd5785ee28018212b147
elsamaitsa <elsamaitsa@gmail.com>
2021-05-20 11:48:59 +0700

    el-edit login

--- application/views/Login.php
+++ application/views/Login.php
@@ -1,42 +1,69 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title></title>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title></title>
 </head>
 
 <body>
-    <div class="wrapper fadeInDown">
-        <div id="formContent">
-            <!-- Tabs Titles -->
-
-            <!-- Icon -->
-            <div class="fadeIn first">
-                <img src="" id="icon" alt="User Icon" />
-            </div>
-
-            <!-- Login Form -->
-            <form method="POST">
-                <!-- <?php if ($pesan = $this->session->flashdata('message')) : ?>
+	<div class="box-login">
+		<div class="row">
+			<div class="col-md-6 col-sm-6 text-center">
+				<img src="<?=base_url()?>assets/image/3071357.jpg" style="width: 67%;">
+				<div class="box-text">
+                    <h6 class="font-weight-bold">Selamat datang,</h6>
+                    <h6 class="font-12">Silahkan login sistem sebagai admin sesuai dengan destinasi yang anda kelola</h6>
+				</div>
+			</div>
+			<div class="col-md-6 col-sm-6 f-poppins box-2">
+				<h3 class="font-weight-bold text-center mb-5">Login</h3>
+				<!-- Login Form -->
+				<form method="POST">
+					<!-- <?php if ($pesan = $this->session->flashdata('message')) : ?>
                     <div class="alert alert-danger alert-dismissible show fade">
                         <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
                         <?= $pesan ?>
                     </div>
                 <?php endif ?> -->
-                <input type="email" id="login" class="fadeIn second" name="email" placeholder="Email" required>
-                <input type="password" id="password" class="fadeIn third" name="password" placeholder="Password" required>
-                <input type="submit" class="fadeIn fourth" value="Log In">
-            </form>
-
-            <!-- Remind Passowrd -->
-            <div id="formFooter">
-                <a class="underlineHover" href="#">Forgot Password?</a>
-            </div>
-
-        </div>
-    </div>
+					<div class="form-group">
+						<label class="label-custom">Username</label>
+						<input type="email" id="login" class="form-control input-custom" name="email"
+							placeholder="ex : admin@admin.co.id" required>
+					</div>
+					<div class="form-group">
+						<label class="label-custom">Password</label>
+						<input type="password" id="password" class="form-control input-custom" name="password"
+							placeholder="Enter Your Password" required>
+						<i class="fa fa-eye hide-pass toggle-password" toggle="#password"></i>
+					</div>
+					<div class="text-center">
+						<input type="submit" class="btn btn-sm btn-info btn-custom" value="Log In">
+					</div>
+				</form>
+				<div class="text-center d-flex mt-5 jc-center">
+					<h6 class="font-10 v-middle">Lupa password ? </h6><a href="modal_lupa" class="font-10 ml-1">Reset
+						Password</a>
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
+<script>
+	$(document).ready(function () {
+		$(".toggle-password").click(function () {
+
+			$(this).toggleClass("fa-eye fa-eye-slash");
+			var input = $($(this).attr("toggle"));
+			if (input.attr("type") == "password") {
+				input.attr("type", "text");
+			} else {
+				input.attr("type", "password");
+			}
+		});
+	});
+
+</script>
 
 </html>
