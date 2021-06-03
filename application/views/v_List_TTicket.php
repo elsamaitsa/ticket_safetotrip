03d07e61db479abbaeb3f39f9df587be9e13a285
elsamaitsa <elsamaitsa@gmail.com>
2021-06-03 09:11:28 +0700

    el-add checking ticket

--- application/views/v_List_TTicket.php
+++ application/views/v_List_TTicket.php
@@ -30,7 +30,14 @@
 				<div class="card-body">
 					<div class="d-flex mb-4">
 						<div class="ml-1">
-							<button class="btn btn-block btn-outline-secondary btn-sm" title="Refresh"><i class="fa fa-sync-alt"></i></button>
+							<button class="btn btn-block btn-outline-secondary btn-sm" title="Refresh"><i
+									class="fa fa-sync-alt"></i></button>
+						</div>
+						<div class="ml-auto">
+							<button type="button" class="btn btn-sm btn-success" data-toggle="modal"
+								data-target="#modal-default">
+								<i class="fas fa-tags mr-1"></i>Check Ticket
+							</button>
 						</div>
 					</div>
 					<table id="example1" class="table table-sm table-striped font-13">
@@ -45,26 +52,34 @@
 						<tbody class="text-center">
 							<?php $no = 0 ?>
 							<?php foreach ($data_Tticket as $value) : ?>
-								<tr>
-									<td><?= ++$no ?></td>
-									<td><?= $value['tbooking_no'] ?></td>
-									<td>
-										<?php if ($value['tticket_status'] == 'dipesan') : ?>
-											<div class="box-label bg-box-dark">Dipesan</div>
-										<?php elseif ($value['tticket_status'] == 'digunakan') : ?>
-											<div class="box-label bg-box-blue">Digunakan</div>
-										<?php elseif ($value['tticket_status'] == 'refund') : ?>
-											<div class="box-label bg-box-green">Refund</div>
-										<?php else : ?>
-											<div class="box-label bg-box-gray">Hangus</div>
-										<?php endif ?>
-									</td>
-									<td>
-										<a class="btn btn-outline-success btn-sm" href="javascript:void(0)" onclick="view_modal(<?= $value['tticket_id'] ?>)"><i class="fas fa-info-circle"></i></a>
-										<a class="btn btn-outline-info btn-sm" data-id="<?= $value['tticket_id'] ?>" data-nobook="<?= $value['tbooking_no'] ?>" data-stat="<?= $value['tticket_status'] ?>" data-toggle="modal" data-target="#EditModal"><i class="fa fa-edit"></i></a>
-										<button class="btn btn-outline-danger btn-sm" data-href="<?= site_url('T_Ticket/Delete/' . $value['tticket_id']); ?>" data-toggle="modal" data-target="#HapusModal" title="Hapus"><i class="fa fa-trash"></i></button>
-									</td>
-								</tr>
+							<tr>
+								<td><?= ++$no ?></td>
+								<td><?= $value['tbooking_no'] ?></td>
+								<td>
+									<?php if ($value['tticket_status'] == 'dipesan') : ?>
+									<div class="box-label bg-box-dark">Dipesan</div>
+									<?php elseif ($value['tticket_status'] == 'digunakan') : ?>
+									<div class="box-label bg-box-blue">Digunakan</div>
+									<?php elseif ($value['tticket_status'] == 'refund') : ?>
+									<div class="box-label bg-box-green">Refund</div>
+									<?php else : ?>
+									<div class="box-label bg-box-gray">Hangus</div>
+									<?php endif ?>
+								</td>
+								<td>
+									<a class="btn btn-outline-success btn-sm" href="javascript:void(0)"
+										onclick="view_modal(<?= $value['tticket_id'] ?>)"><i
+											class="fas fa-info-circle"></i></a>
+									<a class="btn btn-outline-info btn-sm" data-id="<?= $value['tticket_id'] ?>"
+										data-nobook="<?= $value['tbooking_no'] ?>"
+										data-stat="<?= $value['tticket_status'] ?>" data-toggle="modal"
+										data-target="#EditModal"><i class="fa fa-edit"></i></a>
+									<button class="btn btn-outline-danger btn-sm"
+										data-href="<?= site_url('T_Ticket/Delete/' . $value['tticket_id']); ?>"
+										data-toggle="modal" data-target="#HapusModal" title="Hapus"><i
+											class="fa fa-trash"></i></button>
+								</td>
+							</tr>
 							<?php endforeach ?>
 						</tbody>
 					</table>
@@ -131,7 +146,8 @@
 					<input type="hidden" name="id_Tticket" id="id">
 					<div class="form-group">
 						<label class="font-14">Booking No</label>
-						<input type="text" class="form-control font-13" style="background-color: #fff;" name="nobook" id="nobook" placeholder="Booking No" required>
+						<input type="text" class="form-control font-13" style="background-color: #fff;" name="nobook"
+							id="nobook" placeholder="Booking No" required>
 					</div>
 					<div class="form-group">
 						<label class="font-14">Status</label>
@@ -176,19 +192,86 @@
 	</div>
 </div>
 
+
+<!-- Modal Check Ticket -->
+<div class="modal fade" id="modal-default">
+	<div class="modal-dialog">
+		<div class="modal-content" style="width: 80%; margin: auto;">
+			<div class="modal-body">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+				<div class="text-center">
+					<h4 class="font-weight-bold">Checking Ticket</h4>
+					<img src="<?=base_url()?>assets/image/scan-data.jpg" style="width:60%;">
+					<div classs="form-group">
+						<h6 class="text-left font-13">Masukan Nomor Ticket</h6>
+						<input type="text" class="form-control font-13" placeholder="Enter Your Ticket Number">
+					</div>
+
+				</div>
+				<div class="box-note" id="note">
+					<div class="row">
+						<div class="col-sm-6">
+							<h6 class="font-13">ID Ticket</h6>
+						</div>
+						<div class="col-sm-6">
+							<h6 class="font-13"><span class="mr-1">:</span>898989998989</h6>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-6">
+							<h6 class="font-13">User ID</h6>
+						</div>
+						<div class="col-sm-6">
+							<h6 class="font-13"><span class="mr-1">:</span>898989998989</h6>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-6">
+							<h6 class="font-13">Nama Pengunjung</h6>
+						</div>
+						<div class="col-sm-6">
+							<h6 class="font-13"><span class="mr-1">:</span>Nama Visitor</h6>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-sm-6">
+							<h6 class="font-13">Tempat Destinasi</h6>
+						</div>
+						<div class="col-sm-6">
+							<h6 class="font-13"><span class="mr-1">:</span>Labuan Bajo, NTT</h6>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="modal-footer justify-content-between">
+				<button type="button" class="btn btn-sm btn-default" data-dismiss="modal">Close</button>
+				<button type="button" class="btn btn-sm btn-success" id="btn-cek"><i
+						class="fa fa-check mr-1"></i>Cek</button>
+				<button type="button" class="btn btn-sm btn-info" id="btn-cetak"><i
+						class="fa fa-print mr-1"></i>Cetak</button>
+			</div>
+		</div>
+		<!-- /.modal-content -->
+	</div>
+	<!-- /.modal-dialog -->
+</div>
+<!-- /.modal -->
+
 <script>
-	$(document).ready(function() {
+	$(document).ready(function () {
 		table = $("#example1").DataTable({
 			"responsive": true,
 			"autoWidth": false,
 		});
 
-		$('#HapusModal').on('show.bs.modal', function(e) {
+		$('#HapusModal').on('show.bs.modal', function (e) {
 			$(this).find('.btn-ok').attr('href', $(e.relatedTarget).data('href'));
 		});
 
 
-		$('#EditModal').on('show.bs.modal', function(event) {
+		$('#EditModal').on('show.bs.modal', function (event) {
 			var div = $(event.relatedTarget) // Tombol dimana modal di tampilkan
 			var modal = $(this)
 			// Isi nilai pada field
@@ -198,6 +281,20 @@
 			document.querySelector('#stat [value="' + val + '"]').selected = true;
 			document.getElementById("nobook").readOnly = true;
 		});
+
+		$('#note').hide();
+		$('#btn-cetak').hide();
+		$('#btn-cek').click(function () {
+			$('#btn-cetak').show();
+			$('#btn-cek').hide();
+			$('#note').show();
+		});
+
+		$('#btn-cetak').click(function () {
+			$('#btn-cetak').hide();
+			$('#btn-cek').show();
+		});
+
 	});
 
 	function view_modal(tticket_id) {
@@ -206,7 +303,7 @@
 			url: "<?= site_url('T_Ticket/Detail/') ?>" + tticket_id,
 			type: "GET",
 			dataType: "JSON",
-			success: function(data) {
+			success: function (data) {
 				$('#tbooking_no').html(data.tbooking_no);
 				$('#date_booking').html(data.tbooking_date_booking);
 				$('#date_visit').html(data.tbooking_date_visited);
@@ -219,13 +316,14 @@
 	}
 
 
-	setInterval(function() {
+	setInterval(function () {
 		$.ajax({
 			url: "<?= base_url('T_Ticket/CheckTicket') ?>",
 			type: "GET",
-			success: function() {
+			success: function () {
 				console.log("success");
 			},
 		});
 	}, 60000);
+
 </script>
